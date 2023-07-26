1560b5b0c2bc2aa7083af45df503ed6377f2b8f7
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2023-07-26 17:06:23 -0300

    arch: first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { globalCss } from "@ignite-ui/react";
+
+
+export const globalStyles = globalCss({
+    '*': {
+        margin: 0,
+        padding: 0,
+        boxSizing: 'border-box'
+    },
+    
+    body: {
+        color: '$gray100',
+        backgroundColor: '$gray900',
+        'webkit-font-smoothing': 'antialised'
+    }
+})
